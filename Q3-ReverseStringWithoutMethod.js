e9217a442b23f1bb62faabb043b08f3a6246883a
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2026-06-03 19:37:46 +0530

    Update Q3-ReverseStringWithoutMethod.js

--- Q3-ReverseStringWithoutMethod.js
+++ Q3-ReverseStringWithoutMethod.js
@@ -1,1 +1,11 @@
 
+function reverseString(str) {
+  const reverse = "";
+
+  for(let i = length.str - 1; i>=0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
+}
+
+console.log(reverseString('BTS'));

1f146dc78feae86bca04affd398036329573add0
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2026-06-03 20:49:54 +0530

    Update Q5-IsPalindromeCaseSensitive.js

--- Q5-IsPalindromeCaseSensitive.js
+++ Q5-IsPalindromeCaseSensitive.js
@@ -1,1 +1,10 @@
 
+function isPalindrome(str) {
+  let toLowerCase = str.ToLowerCase();
+
+  let palindrome = toLowerCase.split('').reverse().join('');
+
+  return toLowerCase === palindrome;
+}
+
+console.log(isPalindrome("MaDam"));

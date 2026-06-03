9acb9268c70e576a416c03b34afe1fbd758672f6
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2026-06-03 22:53:27 +0530

    Update Q7-FindLargestNum.js

--- Q7-FindLargestNum.js
+++ Q7-FindLargestNum.js
@@ -1,1 +1,13 @@
 
+function findLargest(arr) {
+  let largest = arr[0];
+
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i] > largest) {
+      largest += arr[i];
+    }
+  }
+  return largest;
+}
+
+console.log(findLargest([2,6,4,1,9]));

6167d8ddadabbfe096889314f3553ec102378cab
Ashakiran Jyoti <125113751+ashakiranjyoti@users.noreply.github.com>
2026-06-14 19:05:36 +0530

    Update Q1EvenOdd.js

--- Q1EvenOdd.js
+++ Q1EvenOdd.js
@@ -1,1 +1,12 @@
 
+function isEven(num) {
+    return num % 2 === 0;  // remainder 0 hai to even
+}
+
+function isOdd(num) {
+    return num % 2 !== 0;  // remainder 1 hai to odd
+}
+
+console.log(isEven(4));   // true
+console.log(isOdd(7));    // true
+console.log(isEven(5));   // false
